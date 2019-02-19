a09491b69c930ccbef83e425a8ef79d41f827ab7
jiaqi <2468260248@qq.com>
2019-02-20 00:57:34 +0800

    00点57分

--- js/register.js
+++ js/register.js
@@ -1,78 +1,68 @@
-//验证是否为手机号
-function checkPhone(phone){
-	var dl = /(^[1][0-9]\d{9}$)/;
-	var len = phone.length;
-	//大陆手机号码
-	if(len == 11){
-		if(!dl.exec(phone)){
-			console.log(this.value);
-			$("#phoneTit").innerHTML="请输入正确的手机号";
-			$("#phoneTit").style.display = "block";
-			$("#phone").focus();
+//用户名验证
+$(document).ready(function(){
+	let phone = $("#phone").val();
+$("#phone").onblur=function(){
+	//验证是否为手机号
+	function checeekPhone(phone){
+		var dl = /(^[1][0-9]\d{9}$)/;
+		var len = phone.length;
+		console.log(phone);
 
-		}else{
-			$("#phoneTit").style.display="hidden";
-			checkuser(this.value);
-		}
-	}else{
-        $("#phone").focus();
-		$("#phoneTit").innerHTML="请输入11位手机号";
-		$("#phoneTit").style.display = "block";
-	}
-}
-
-//验证用户名是否存在
-function checkuser(name){
-	let xhr = new XMLHttpRequest();		
-	//2、设置请求参数
-	xhr.open('get','php/checkUser.php?username='+name,true);
-	//3、设置回调函数
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState==4 && xhr.status==200){		
-				if(xhr.responseText=='1'){
-					$("#phoneTit").style.display = "block";
-					$("#phoneTit").innerHTML="<font style='color:#A5C11B'>恭喜你，可以使用该手机号！</font>";
-				}else{
-					$("#phoneTit").style.display = "block";
-					$("#phoneTit").innerHTML="<font style='color:#F63B21'>手机号已存在,请更换手机号!</font>";
-				}
+		//大陆手机号码
+		if(len == 11){
+			if(!dl.exec(phone)){
+				console.log(phone);
+				$("#phoneTit").html("请输入正确的手机号");		
+				$("#phone").focus();
+	
+			}else{
+				checkUser($(phone));
 			}
+		}else{
+			$("#phone").focus();
+			$("#phoneTit").html("请输入11位手机号");
 		}
-	xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-	//4、发送
-	xhr.send();
-}
-
-//验证确认密码
-(function ckPwd(obj) {	
-	$("#password").onblur = function () {
-	var userpwd = $('#password').value;
+	}
+	function checkUser(phone) { 
+		console.log(phone);
+		$.get("../php/checkUser.php", phone,
+			function (data) {
+				data == "0"?$("#phoneTit").html("用户名已存在，请更换用户名"):$("#phoneTit").html("<font style='color:#A5C11B'>恭喜你，可以使用该用户名！</font>");
+			}
+		);
+	 }
+};
+})
+//验证密码
+function ckPwd1(obj) { 
+	var userpwd = obj.value;
 	var len = jmz.GetLength(rtrim(userpwd));
-		if(len<6 || len>16) {  
-			$("#pswTit").innerHTML="<font style='color:#F63B21'>密码长度为6-16个字符，区分大小写</font>";
-			$("#pswTit").style.display = "block";
-			return false;  
-		}else{
-			$("#pswTit").style.display = "none";
-		}
-	}
-	$("#repassword").onblur = function (){
-	var userpwd = $('#password').value;
-	var userrpwd = $('#repassword').value;
-		if(userpwd != userrpwd) {  
-			$("#rePswTit").innerHTML="<font style='color:#F63B21' >密码不一致，请重新输入</font>";
-			$("#rePswTit").style.display = "block";  
-			return false;  
-		}else{
-			$("#rePswTit").style.display = "none";
-
-		}
-	}
-})();
-
-$("#phone").onblur = function(){
-	checkPhone($("#phone").value);
+    if(len<6 || len>16) {  
+    	$("#pswTit").html("<font style='color:#F63B21'>密码长度为6-16个字符，区分大小写</font>");  
+        return false;  
+    }else{  
+    	$("#pswTit").html("<font style='color:#A5C11B'>密码可用</font>");  
+        return true;  
+    }  
 }
+//验证确认密码
+function ckPwd2(obj) {
+	var userpwd = $('#password').val();
+	var userrpwd = $('#repassword').val();
+	var len = jmz.GetLength(rtrim(userrpwd));
+    if(len<6 || len>16) {  
+    	$("#rePswTit").html("<font style='color:#F63B21'>密码长度为6-16个字符，区分大小写</font>");   
+        return false;  
+    }else if(userpwd != userrpwd) {  
+    	$("#rePswTit").html("<font style='color:#F63B21' >密码不一致，请重新输入</font>");  
+        return false;  
+    }else{  
+    	$("#rePswTit").html("<font style='color:#A5C11B'>密码一致，请继续</font>");  
+        return true;  
+    }  
+} 
+
+
 
 function zhuche() {
     var str_phone = document.getElementById("phone").value;
